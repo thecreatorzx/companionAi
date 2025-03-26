9da39366f319fda6181b4b9668279296c5843aba
thecreatorzx <saadyesican@gmail.com>
2025-03-26 12:02:53 +0530

    updated requirements

--- sentiment.py
+++ sentiment.py
@@ -4,8 +4,10 @@
 # print(sentiment_pipeline(data))
 
 
-
+import time
 from transformers import pipeline
+print(time.ctime(time.time()))
 classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion', return_all_scores=True)
 prediction = classifier("I love using transformers. The best part is wide range of support and its easy to use", )
 print(prediction)
+print(time.ctime(time.time()))
